87b778155fa0d80f6abbd39cb61678b916ed00b1
Otema Aaron <aaron.otema@gmail.com>
2021-06-26 12:13:37 -0700

    Initial commit

---
+++ src/main/java/com/example/carrecordsdatabaseapisystem/CarRecordsDatabaseApiSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrecordsdatabaseapisystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRecordsDatabaseApiSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRecordsDatabaseApiSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carrecordsdatabaseapisystem/CarRecordsDatabaseApiSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carrecordsdatabaseapisystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRecordsDatabaseApiSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
